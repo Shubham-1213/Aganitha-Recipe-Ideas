c167eed6ae1d047235c653840cd84d413bd6cdf1
Shubham-1213 <202151153@iiitvadodara.ac.in>
2024-12-15 21:53:00 +0530

    fixed bugs

--- src/pages/SearchResult/SearchResult.jsx
+++ src/pages/SearchResult/SearchResult.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-// import MealCard from "../../components/MealCard/MealCard";
 import Loader from "../../components/Loader/Loader";
 import Card from "../../components/Card/Card"
 import "../../components/MealCard/meal.css"
